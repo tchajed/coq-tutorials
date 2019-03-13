fa0b17462f33390cbab3e74ca4dbe0d2eb11cb34
Tej Chajed <tchajed@mit.edu>
2019-03-13 09:40:06 -0400

    Add addition flattening worked example

--- src/Reflection.v
+++ src/Reflection.v
@@ -32,8 +32,95 @@ a proof about some [a:A] that appears in the goal. *)
 
 (** ** A motivating example **)
 
-(** The best way to understand proof by reflection is to look at an example. *)
+(** The best way to understand proof by reflection is to look at an example.
+We'll use reflection to normalize an addition expression. *)
 
+Require Import Coq.Arith.PeanoNat.
+
+Inductive term :=
+| Const (n:nat)
+| Add (e1 e2:term).
+
+Fixpoint denote (t:term) : nat :=
+  match t with
+  | Const n => n
+  | Add e1 e2 => denote e1 + denote e2
+  end.
+
+Fixpoint flatten (x0:term) (t:term) : term :=
+  match t with
+  | Const n => Add x0 (Const n)
+  | Add e1 e2 => flatten (flatten x0 e1) e2
+  end.
+
+Eval cbn [denote flatten] in
+    fun x y z w => denote (flatten (Const 0)
+                                (Add (Add (Const x) (Const y))
+                                     (Add (Const z) (Const w)))).
+
+(* the property we'll prove is that flattening does not change the denotation of
+a term; to do so we need to first prove a more general property to get the
+induction to go through *)
+
+Theorem flatten_sound_general t : forall x,
+  denote (flatten x t) = denote x + denote t.
+Proof.
+  induction t; simpl; intros; auto.
+  rewrite IHt2, IHt1.
+  rewrite Nat.add_assoc; auto.
+Qed.
+
+Theorem flatten_sound t :
+  denote t = denote (flatten (Const 0) t).
+Proof.
+  rewrite flatten_sound_general; simpl; auto.
+Qed.
+
+Ltac reify e :=
+  match e with
+  | ?x + ?y =>
+    let r_x := reify x in
+    let r_y := reify y in
+    constr:(Add r_x r_y)
+  | _ => constr:(Const e)
+  end.
+
+Theorem demo x y z :
+  x + (y + z) = x + y + z.
+Proof.
+  match goal with
+  | |- ?e = ?e' => let t := reify e in
+                 let t' := reify e' in
+                 change (denote t = denote t')
+  end.
+  match goal with
+  | |- denote ?t = denote ?t' =>
+    rewrite (flatten_sound t), (flatten_sound t')
+  end.
+  (* this is where the magic becomes apparent: the soundness theorem applies to
+  any term, but flatten computes to a particular expression when applied to
+  these concrete terms *)
+  cbn [denote flatten].
+  reflexivity.
+Qed.
+
+(* we can wrap up the process of simplifying expressions in a tactic *)
+Ltac canonical_assoc e :=
+  let t := reify e in
+  change e with (denote t);
+  rewrite (flatten_sound t);
+  cbn [denote flatten].
+
+Theorem larger_demo x y z w :
+  x + (y + z + w) + x + (y + z) =
+  x + (y + z) + (w + (x + y)) + z.
+Proof.
+  match goal with
+  | |- ?e = ?e' => canonical_assoc e;
+                   canonical_assoc e'
+  end.
+  reflexivity.
+Qed.
 
 (** ** Why use reflection? **)
 
